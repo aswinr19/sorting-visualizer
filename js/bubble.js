3fb4d4336ab6b91b7e804a38508c9b33877afe71
Aswin r <aswinr019@gmail.com>
2022-06-19 15:11:17 +0530

    added functionality to change the number of bars using slider and re-render when its changed

---
+++ js/bubble.js
@@ -0,0 +1,15 @@
+const swap = (e1, e2) => {
+  let temp = e1;
+  e1 = e2;
+  e2 = temp;
+};
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      swap(array[i], array[i + 1]);
+    }
+  }
+
+  return array;
+};
--- js/index.js
+++ js/index.js
@@ -1,28 +1,37 @@
 let array = [];
 
-const generateArray = () => {
+const range = document.querySelector(".range");
 
-    for (let i = 0; i < 200; i++) {
+// console.log(range.value);
 
-        let random = Math.floor(Math.random() * 100);
-      
-        array.push(random);
-      }
 
+const clearArray= (array) => {
+  while (array.length) {
+    array.pop();
+  }
 }
 
-generateArray();
+const generateArray = () => {
 
-const newArray = document.querySelector(".generate");
+ 
+  clearArray(array);
 
-newArray.addEventListener("click",generateArray());
+  for (let i = 0; i < range.valueAsNumber; i++) {
+    let random = Math.floor(Math.random() * 100);
 
-const bars = document.querySelector("#bars");
+    array.push(random);
+  }
+  console.log(array);
+  const bars = document.querySelector("#bars");
 
-array.forEach((value, index) => {
+  bars.innerHTML = '';
 
-  bars.innerHTML += `<div class="bar" style=height:${value*5}px></div>`;
+  array.forEach((value, index) => {
+    bars.innerHTML += `<div class="bar" style=height:${value * 5}px></div>`;
+  });
+ 
+}
 
+ generateArray();
 
-
-});
+let newArray = document.querySelector("#generate");
