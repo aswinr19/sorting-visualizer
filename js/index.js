a763ccf1138a4124d98f8baa9c70409c682cfaea
Aswin r <aswinr019@gmail.com>
2022-06-22 21:38:28 +0530

    minor changes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-
 let array = [];
 
 const range = document.querySelector(".range");
 
-const bars = document.querySelector("#bars");
+const bar = document.querySelector("#bar");
 
 const newArray = document.querySelector("#generate");
 
@@ -22,25 +21,36 @@ const generateRandomArray = (array, range) => {
   }
 
   console.log(array);
-  bars.innerHtml = "";
+  bar.innerHtml = "";
 
   array.forEach((value) => {
-        bars.innerHTML += `<div class="bar" style=height:${value * 5}px></div>`;
+    bar.innerHTML += `<div class="bars" id="${value}"style=height:${
+      value * 5
+    }px></div>`;
   });
-
 };
 
 generateRandomArray(array, range);
 
+const swap = (e1, e2) => {
+  let temp = e1.style.height;
+  e1.style.height = e2.style.height;
+  e2.style.height = temp;
+};
 
-//bubble sort 
+const bubbleSort = (array) => {
+  let bars = document.querySelectorAll(".bars");
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (parseInt(bars[j].style.height) > parseInt(bars[j + 1].style.height)) {
+        // bars[j].style.background = "red";
+        // bars[j + 1].style.background = "red";
 
-const bubbleSortArray = ( array ) => {
-
-  for(let i = 0; i < array.length; i++){
-
-      // for(let j = 0; j < )
-
+        swap(bars[j] > bars[j + 1]);
+      }
+    }
   }
+};
 
-}
+
+bubbleSort(array);
