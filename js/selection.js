0ebd7e0c24935abbd00967f13887ed9c9a6a2c83
Aswin r <aswinr019@gmail.com>
2022-06-29 20:02:42 +0530

    selection sort working

--- js/selection.js
+++ js/selection.js
@@ -1,1 +1,31 @@
+async function selectionSort() {
+  console.log("in selection sort");
+  const element = document.querySelectorAll(".bar");
+  let i;
+  let j;
+  for (i = 0; i < element.length - 1; i++) {
+    let min = i;
+    for (j = i+1; j < element.length; j++) {
+      element[min].style.background = "red";
+      element[j].style.background = "red";
+      if (parseInt(element[j].style.height) < parseInt(element[min].style.height)) min = j;
+      element[min].style.background = "yellow";
+      element[j].style.background = "yellow";
+    }
+
+    await waitFor(millisec);
+    swap(element[i], element[min]);
+     element[i].style.background = "green";
+  }
+  element[element.length-1].style.background = "green";
+}
+
 const selectionSortBtn = document.querySelector("#selection");
+
+selectionSortBtn.addEventListener("click", async function () {
+  console.log("clicked");
+
+  disableSortingBtns();
+  await selectionSort();
+  enableSortingBtns();
+});
