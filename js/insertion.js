5773040100e5f70af4febb8f79ee170744abc645
Aswin r <aswinr019@gmail.com>
2022-06-30 21:40:14 +0530

    insertion sort working

--- js/insertion.js
+++ js/insertion.js
@@ -3,23 +3,30 @@ async function insertionSort() {
   console.log("In insertion sort");
   const element = document.querySelectorAll(".bar");
 
-  for (let i = 1; i < element.length ; i++) {
+  for (let i = 1; i < element.length; i++) {
     value = element[i].style.height;
     let hole = i;
     while (
       hole > 0 &&
       parseInt(element[hole - 1].style.height) > parseInt(value)
     ) {
-     
+      element[i].style.background = "red";
+      element[hole - 1].style.background = "red";
+      await waitFor(millisec);
       element[hole].style.height = element[hole - 1].style.height;
    
+      element[hole - 1].style.background = "yellow";
+
       hole--;
     }
     await waitFor(millisec);
     element[hole].style.height = value;
-    element[i].style.background = "green";
+   
   }
-  element[0].style.background = "green";
+  for (let i = 0; i < element.length; i++) {
+    await waitFor(millisec);
+  element[i].style.background = "green";
+  }
 }
 
 //accessing insertion sort button.
