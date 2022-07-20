34f92829901ee65711083c5c5d884cb043add83b
aswinr <aswinr2442002@gmail.com>
2022-07-20 16:10:34 +0530

    merge sort template removed

--- js/merge.js
+++ js/merge.js
@@ -1,39 +1,3 @@
-async function merge(leftArr,rightArr){
-	const output = [];
-	let leftIndex = 0;
-	let rightIndex = 0;
-
-	while(leftIndex < leftArr.length && rightIndex < rightArr.length){
-		const lefftEl = leftArr[leftIndex];
-		const rightEl = rightArr[rightIndex];
-
-		if(leftEl < rightEl){
-			output.push(leftEl);
-			lefftIndex++;
-		}
-		else 
-		{
-			output.push(rightEl);
-			rightIndex++;
-		}
-	}
-	return [...output , ...leftArr.slice(lefftIndex) , ...rightArr.slice(rightIndex)];
-}
-
-
-async function mergeSort(element){
-	if(element.length <= 1){
-		return ;
-	}
-	const middleIndex = Math.floor(array.length/2);
-	const leftArr = element.slice(0,middleIndex);
-	const rightArr = element.slice(middleIndex);
-	return merge(
-		mergeSort(leftArr);
-		mergeSort(rightArr);
-	);
-}
-
 
 
 
@@ -44,12 +8,10 @@ mergeSortBtn.addEventListener("click", async function () {
 
   const element = document.querySelectorAll(".bar");
 
-  const low = 0;
-  const high = element.length - 1;
   disableNewArrayBtn();
   disableSizeSlider();
   disableSortingBtns();
-  await mergeSort(element, low, high);
+  await mergeSort(element);
   enableNewArrayBtn();
   enableSizeSlider();
   enableSortingBtns();
